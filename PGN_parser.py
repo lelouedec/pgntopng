cbc7129a5c75a3fdfbcfe0cc062b0631659f085c
lelouedec <justin.lelouedec@gmail.com>
2018-05-11 12:59:47 +0200

    fixing bugs for rooks

--- PGN_parser.py
+++ PGN_parser.py
@@ -3,7 +3,7 @@ from PIL import Image
 from Utils import *
 from create_gif import *
 import sys
-
+import re
 def create_game_array(gamestring):
     game_array = gamestring.split(" ")
     game_array2 = []
@@ -31,29 +31,31 @@ positions = [[(0,7),(1,7),(2,7),(3,7),(4,7),(5,7),(6,7),(7,7)],[(0,0),(1,0),(2,0
 #positions[1] = main pieces blacks
 #positions[2] = pawns pieces white
 #positions[3] = pawns pieces blacks
-
-def generate(game,game_name):
+arg1 = 1
+arg2 = 1
+def generate(game):
     gameboard = []
     gameboard.append(positions)
     images = []
+    check  = []
+    if(int(arg1)):
+        if(arg2):
+            image = Image.new( 'RGB', (800,800), (181,136,99)) # create a new black image
+        else:
+            image = Image.new( 'RGB', (1400,800), (181,136,99)) # create a new black image
+        img1 = draw_board(positions,image)
+        image.paste(img1,(0,0))
+        images.append(image)
     for i in range(0,len(game)-1):
-        print(i)
-        if(int(arg1)):
-            if(arg2):
-                image = Image.new( 'RGB', (800,800), (181,136,99)) # create a new black image
-            else:
-                image = Image.new( 'RGB', (1400,800), (181,136,99)) # create a new black image
-            img1 = draw_board(positions,image)
-            image.paste(img1,(0,0))
-            images.append(image)
-        ###WHITE MOVE
-
         for j in range(0,2):
-            if(game[i][j][-1] == "+" or game[i][j][-1] == "+"):#check
+            if(game[i][j][-1] == "+" or game[i][j][-1] == "#"):#check
                 a = list(game[i])
                 a[j] = a[j][:-1]
-                print(a)
                 game[i] = tuple(a)
+                if(j==0):
+                    check.append((i,j, positions[1][4] ))
+                else:
+                    check.append((i,j, positions[0][4] ))
             if(len(game[i][j])==2):#can only be a pawn moving vertically
                 coord = transform_board_to_screen(game[i][j])
                 if(j==0):
@@ -73,11 +75,16 @@ def generate(game,game_name):
                     piece_index = get_piece_index(game[i][j][0],(game[i][j][1],game[i][j][2]),j,positions)
                     positions[j][piece_index] = transform_board_to_screen((game[i][j][1],game[i][j][2]))
             elif(len(game[i][j])==4):#Nbd7 cxb5 Nxd6
+                if('=' in game[i][j]):
+                    print("Pawn promotion, exiting game")
+                    return images,checks
                 if(game[i][j][1] =='x'):#cxb5 Nxd6
                     piece_index = get_piece_index(game[i][j][0],(game[i][j][2],game[i][j][3]),j,positions)
                     goal = transform_board_to_screen( (game[i][j][2],game[i][j][3]) )
                     if(piece_index == -1):#cxb5
                         dead_piece = get_dying_piece((game[i][j][2],game[i][j][3]),positions)
+                        if(dead_piece == None):
+                            return images,check
                         if(j==0):
                             moved = get_pawn_moved((dict[game[i][j][0]],goal[1]+1),positions)
                         else:
@@ -85,19 +92,39 @@ def generate(game,game_name):
                         positions[moved[0]][moved[1]] = goal
                         positions[dead_piece[0]][dead_piece[1]] = (-1,-1)
                     else:#Nxd6
-                        piece_dead_indices = get_dying_piece((game[i][j][2],game[i][j][3]),positions)
-                        positions[piece_dead_indices[0]][piece_dead_indices[1]] = (-1,-1)
+                        dead_piece = get_dying_piece((game[i][j][2],game[i][j][3]),positions)
+                        if(dead_piece == None):
+                            return images,check
+                        positions[dead_piece[0]][dead_piece[1]] = (-1,-1)
                         positions[j][piece_index] = goal
                 else:#Nbd7
                     piece_index = get_piece_concurrence(game[i][j][0],game[i][j][1],j,positions)
                     goal = transform_board_to_screen((game[i][j][2],game[i][j][3]))
                     positions[j][piece_index] = goal
             else:
-                if(game[i][j][0]=='O'):#O-O-O
+                if('=' in game[i][j]):
+                    print("Pawn promotion, exiting game")
+                    return images,check
+                elif(game[i][j][0]=='O'):#O-O-O
                     positions[j][0] = (positions[j][0][0]+3,positions[j][0][1])
                     positions[j][4] = (positions[j][4][0]-2,positions[j][4][1])
+                elif(game[i][j][2]=='x'):
+                    move = game[i][j]
+                    move = move[:2]+move[3:]
+                    ##find which piece to move
+                    piece_index = get_piece_concurrence(move[0],move[1],j,positions)
+                    ##goal to reach
+                    goal = transform_board_to_screen((move[2],move[3]))
+                    ##find dead piece
+                    dead_piece = get_dying_piece((move[2],move[3]),positions)
+                    if(dead_piece == None):
+                        return images,check
+                    ##kill it
+                    positions[dead_piece[0]][dead_piece[1]] = (-1,-1)
+                    ##change killer position
+                    positions[j][piece_index] = goal
                 else:
-                    print(game[i])
+                    print(" failed " + str(game[i]))
 
             if(int(arg1)):
                 if(arg2):
@@ -110,7 +137,7 @@ def generate(game,game_name):
 
 
 
-    return images
+    return images,check
 def ensure_dir(file_path):
     directory = os.path.dirname(file_path)
     if not os.path.exists(directory):
@@ -136,36 +163,63 @@ def save_images(path,images):
         else:
             new_im.save(path+str(j)+".png", "PNG")
 
+def load_games(start,nb,file):
+    lines = []
+    count = 0
+    with open(file) as infile:
+        for line in infile:
+            val =re.sub(r' {[^}]*}','',line)
+            if(val[0] =='1'):
+                if(count >= start):
+                    lines.append(val)
+                    if(count>nb):
+                        break
+                count = count + 1
+    return lines
 if __name__ == '__main__':
     arg1 = 0
     arg2 = 0
     game_string = "1. e4 e5 2. Nf3 Nc6 3. Bb5 a6 4. Ba4 Nf6 5. O-O Be7 6. Re1 b5 7. Bb3 d6 8. c3 O-O 9. h3 Nb8 10. d4 Nbd7 11. c4 c6 12. cxb5 axb5 13. Nc3 Bb7 14. Bg5 b4 15. Nb1 h6 16. Bh4 c5 17. dxe5 Nxe4 18. Bxe7 Qxe7 19. exd6 Qf6 20. Nbd2 Nxd6 21. Nc4 Nxc4 22. Bxc4 Nb6 23. Ne5 Rae8 24. Bxf7+ Rxf7 25. Nxf7 Rxe1+ 26. Qxe1 Kxf7 27. Qe3 Qg5 28. Qxg5 hxg5 29. b3 Ke6 30. a3 Kd6 31. axb4 cxb4 32. Ra5 Nd5 33. f3 Bc8 34. Kf2 Bf5 35. Ra7 g6 36. Ra6+ Kc5 37. Ke1 Nf4 38. g3 Nxh3 39. Kd2 Kb5 40. Rd6 Kc5 41. Ra6 Nf2 42. g4 Bd3 43. Re6 1/2-1/2"
     game2_string = "1. e4 c6 2. c4 d5 3. exd5 cxd5 4. cxd5 Nf6 5. Nc3 g6 6. Bc4 Bg7 7. Nf3 O-O 8. O-O Nbd7 9. d3 Nb6 10. Qb3 Bf5 11. Re1 h6 12. a4 Nfd7 13. Be3 a5 14. Nd4 Nxc4 15. dxc4 Nc5 16. Qa3 Nd3 17. Nxf5 gxf5 18. Red1 Ne5 19. b3 Ng4 20. Qc1 f4 21. Bd4 Bxd4 22. Rxd4 e5 23. Rd2 Qh4 24. h3 Nf6 25. Qe1 Qg5 26. Ne4 Nxe4 27. Qxe4 f5 28. Qxe5 Rae8 29. h4 Qxh4 30. Qc3 Re4 31. d6 Qg5 32. f3 Re3 33. Qxa5 Rfe8 34. Rf2 Qf6 35. Rd1 R3e5 36. d7  1-0"
-    game3_string = "1. e4 e6 2. d4 d5 3. Nd2 c5 4. exd5 Qxd5 5. Ngf3 cxd4 6. Bc4 Qd6 7. Qe2 Nf6 8. Nb3 Nc6 9. Bg5 Qb4+ 10. Bd2 Qb6 11. O-O-O Bd7 12. Bg5 Bc5 13. Kb1 O-O-O 14. Ne5 Nxe5 15. Qxe5 Bd6 16. Qe2 h6 17. Bd2 Bb4 18. Bxb4 Qxb4 19. Nxd4 Kb8 20. f4 Qc5 21. Nf3 Ng4 22. Rd4 Bc8 23. Rhd1 Rxd4 24. Nxd4 e5 25. h3 exd4 26. hxg4 Bxg4 27. Qxg4 Qxc4 28. Qxg7 Rc8 29. Qe5+ Ka8 30. Qe4 a6 31. b3 Qc6 32. Qxc6 Rxc6 33. f5 Rd6 34. Kc1 Ka7 35. Kd2 Kb6 36. Re1 Rd8 37. g4 Rg8 38. Re4 Kc5 39. Kd3 h5 40. gxh5 Rg3+ 41. Kd2 Rh3 42. f6 Rxh5 43. Re7 Rf5 44. Rxf7 b5 45. Rf8 Kc6 46. Kd3 Rf4 47. a3 Kc7 48. a4 Kb6 49. Ke2 Rf5 50. Kd3 Rf4 51. axb5 axb5 52. c3 dxc3 53. Kxc3 Kc6 54. b4 Kc7 55. f7 Kb7 56. Kd3 Rf3+ 57. Ke4 Rf1 58. Ke5 Rf2 59. Ke6 Re2+ 60. Kd6  Rf2 61. Kc5 1/2-1/2"
-    game_file = ""
     if(len(sys.argv)>1):
         arg1 = sys.argv[1]
     if(len(sys.argv)>2):
         arg2 = sys.argv[2]
 
     # game = create_game_array(game_string)
-    # images = generate(game,"game1")
+    # images = generate(game)
     # path = "./images/game1/configs/"
     # ensure_dir(path)
     # save_images(path,images)
+    #
+    #
     # positions = [[(0,7),(1,7),(2,7),(3,7),(4,7),(5,7),(6,7),(7,7)],[(0,0),(1,0),(2,0),(3,0),(4,0),(5,0),(6,0),(7,0)],
-    #             [(0,6),(1,6),(2,6),(3,6),(4,6),(5,6),(6,6),(7,6)],[(0,1),(1,1),(2,1),(3,1),(4,1),(5,1),(6,1),(7,1)]]
+    #              [(0,6),(1,6),(2,6),(3,6),(4,6),(5,6),(6,6),(7,6)],[(0,1),(1,1),(2,1),(3,1),(4,1),(5,1),(6,1),(7,1)]]
     # game = create_game_array(game2_string)
-    # images = generate(game,"game2")
+    # images = generate(game)
     # path = "./images/game2/configs/"
     # ensure_dir(path)
     # save_images(path,images)
 
+    start = 0
+    nb = start + 4000
+    mines = load_games(start,nb,"./marchgames.pgn")
+    game_counter = start
 
-    positions = [[(0,7),(1,7),(2,7),(3,7),(4,7),(5,7),(6,7),(7,7)],[(0,0),(1,0),(2,0),(3,0),(4,0),(5,0),(6,0),(7,0)],
-                 [(0,6),(1,6),(2,6),(3,6),(4,6),(5,6),(6,6),(7,6)],[(0,1),(1,1),(2,1),(3,1),(4,1),(5,1),(6,1),(7,1)]]
-    game = create_game_array(game3_string)
-    images = generate(game,"game3")
-    path = "./images/game3/configs/"
-    ensure_dir(path)
-    save_images(path,images)
+    for games in mines:
+        positions = [[(0,7),(1,7),(2,7),(3,7),(4,7),(5,7),(6,7),(7,7)],[(0,0),(1,0),(2,0),(3,0),(4,0),(5,0),(6,0),(7,0)],
+                     [(0,6),(1,6),(2,6),(3,6),(4,6),(5,6),(6,6),(7,6)],[(0,1),(1,1),(2,1),(3,1),(4,1),(5,1),(6,1),(7,1)]]
+        print(games)
+        try:
+            game = create_game_array(games)
+            images,checks = generate(game)
+            if(images !=None):
+                path = "./images/game"+str(game_counter)+"/configs/"
+                ensure_dir(path)
+                save_images(path,images)
+                with open("./images/game"+str(game_counter)+"/checks.txt", 'w') as file_handler:
+                    for item in checks:
+                        file_handler.write("{}\n".format(item))
+                game_counter = game_counter + 1
+        except KeyError:
+            print("wrong format")
--- Utils.py
+++ Utils.py
@@ -18,29 +18,32 @@ def get_case_color(case):
         return 0
     else:
         return 1
-def something_in_middle_h(coord,goal,positions):
-    for i in range (0,len(positions)):
-        for j in range(0,8):
-            if(positions[i][j][0] != -1):
-                if(positions[i][j][1] == coord[1]):
-                    if(coord[0] > goal[0]):
-                        if(positions[i][j][0]> goal[0] and positions[i][j][0] < coord[0] ):
-                            return True
-                    else:
-                        if(positions[i][j][0] < goal[0] and positions[i][j][0] > coord[0] ):
-                            return True
+def something_in_middle_v(coord,goal,positions):
+    if(coord[1]< goal[1]):
+        for i in range (0,len(positions)):
+            for j in range(0,8):
+                if(positions[i][j][0] == coord[0] and positions[i][j] != goal and positions[i][j][1]<goal[1]  and positions[i][j][1]>coord[1] and positions[i][j] != coord):
+                    return True
+
+    else:
+        for i in range (0,len(positions)):
+            for j in range(0,8):
+                if(positions[i][j][0] == coord[0]  and positions[i][j] != goal and positions[i][j][1]<coord[1] and positions[i][j][1]>goal[1] and positions[i][j] != coord):
+                    return True
     return False
 
-def something_in_middle_v(coord,goal,positions):
-    for i in range (0,len(positions)):
-        for j in range(0,8):
-            if(positions[i][j][0] == coord[0]):
-                if(coord[1] > goal[1]):
-                    if(positions[i][j][1]> goal[1] and positions[i][j][1] < coord[1] ):
-                        return True
-                else:
-                    if(positions[i][j][0] < goal[1] and positions[i][j][1] > coord[1] ):
-                        return True
+def something_in_middle_h(coord,goal,positions):
+    if(coord[0]<goal[0]): ##horizontally before
+        for i in range (0,len(positions)):
+            for j in range(0,8):
+                if(positions[i][j][1] == coord[1] and positions[i][j] != goal and positions[i][j][0]<goal[0]  and positions[i][j][0]>coord[0] and positions[i][j] != coord):
+                    return True
+
+    else:
+        for i in range (0,len(positions)):
+            for j in range(0,8):
+                if(positions[i][j][1] == coord[1]  and positions[i][j] != goal and positions[i][j][0]<coord[0] and positions[i][j][0]>goal[0] and positions[i][j] != coord):
+                    return True
     return False
 def get_pawn_moved(coord,positions):
     for i in range (0,len(positions)):
@@ -55,6 +58,8 @@ def get_dying_piece(pos,positions):
         for j in range(0,8):
             if(positions[i][j] == coord):
                 return (i,j)
+    print("piece at : "+ str(pos) +", "+str(coord) + " not found" )
+    return None
 
 def get_piece_concurrence(letter,diff,color,positions):
     if(letter=='R'):
@@ -72,7 +77,6 @@ def get_piece_concurrence(letter,diff,color,positions):
                 else:
                     return 7
         else:
-            print(diff)
             if(diff.isalpha()):
                 x_pos=dict[diff]
                 if (positions[1][0][0] == x_pos):
@@ -81,7 +85,6 @@ def get_piece_concurrence(letter,diff,color,positions):
                     return 7
             else:
                 y_pos = 8-int(diff)
-                print(y_pos)
                 if (positions[1][0][1] == y_pos):
                     return 0
                 else:
@@ -150,110 +153,57 @@ def get_piece_concurrence(letter,diff,color,positions):
 
 
 def get_piece_index(letter,but,color,positions):
-    if(letter=='R'):#no possible ambiguity
-        if(color==0):
-            if(positions[0][0] == (-1,-1) and positions[0][7] == (-1,-1)):
-                return -1
-            elif(positions[0][0] == (-1,-1)):
-                return 7
-            elif(positions[0][7] == (-1,-1)):
-                return 0
-            else:
-                coord = positions[0][0]
-                goal = transform_board_to_screen(but)
-                if(coord[0]==goal[0]):
-                    if(something_in_middle_v(coord,goal,positions)):
-                        return 7
-                    else:
-                        return 0
-                elif(coord[1]==goal[1]):
-                    if(something_in_middle_h(coord,goal,positions)):
-                        return 7
-                    else:
-                        return 0
-                else:
-                    return 7
+    if(letter=='R'):
+        if(positions[color][0] == (-1,-1) and positions[color][7] == (-1,-1)):
+            return -1
+        elif(positions[color][0] == (-1,-1)):
+            return 7
+        elif(positions[color][7] == (-1,-1)):
+            return 0
         else:
-            if(positions[1][0] == (-1,-1) and positions[1][7] == (-1,-1)):
-                return -1
-            elif(positions[1][0] == (-1,-1)):
-                return 7
-            elif(positions[1][7] == (-1,-1)):
-                return 0
-            else:
-                coord = positions[1][0]
-                goal = transform_board_to_screen(but)
-                if(coord[0]==goal[0]):
-                    if(something_in_middle_v(coord,goal,positions)):
-                        return 7
-                    else:
-                        return 0
-                elif(coord[1]==goal[1]):
-                    if(something_in_middle_h(coord,goal,positions)):
-                        return 7
-                    else:
-                        return 0
-                else:
+            coord = positions[color][0]
+            goal = transform_board_to_screen(but)
+            if(coord[0]==goal[0]):
+                if(something_in_middle_v(coord,goal,positions)):
                     return 7
+                else:
+                    return 0
+            elif(coord[1]==goal[1]):
+                if(something_in_middle_h(coord,goal,positions)):
+                    return 7
+                else:
+                    return 0
+            else:
+                return 7
     elif (letter=='N'):#if this is a knight moving
-        if(color==0):
-            if(positions[0][1] == (-1,-1) and positions[0][6] == (-1,-1)):#no knight to move
-                return -1
-            else:
-                if(positions[0][1] == (-1,-1)):#only one knight to move
-                    return 6
-                elif (positions[0][6] == (-1,-1)):#only one knight to move
-                    return 1
-                else:#decide which one can be moved here
-                    coord = positions[0][1]
-                    goal = transform_board_to_screen(but)
-                    if(goal == (coord[0]+1,coord[1]+2)):
-                        return 1
-                    elif (goal == (coord[0]+2,coord[1]+1)):
-                        return 1
-                    elif (goal == (coord[0]+2,coord[1]-1)):
-                        return 1
-                    elif (goal == (coord[0]+1,coord[1]-2)):
-                        return 1
-                    elif (goal == (coord[0]-1,coord[1]-2)):
-                        return 1
-                    elif (goal == (coord[0]-2,coord[1]-1)):
-                        return 1
-                    elif (goal == (coord[0]-2,coord[1]+1)):
-                        return 1
-                    elif (goal == (coord[0]-1,coord[1]+2)):
-                        return 1
-                    else:
-                        return 6
+        if(positions[color][1] == (-1,-1) and positions[color][6] == (-1,-1)):#no knight to move
+            return -1
         else:
-            if(positions[1][1] == (-1,-1) and positions[1][6] == (-1,-1)):#no knight to move
-                return -1
-            else:
-                if(positions[1][1] == (-1,-1)):#only one knight to move
-                    return 6
-                elif (positions[1][6] == (-1,-1)):#only one knight to move
+            if(positions[color][1] == (-1,-1)):#only one knight to move
+                return 6
+            elif (positions[color][6] == (-1,-1)):#only one knight to move
+                return 1
+            else:#decide which one can be moved here
+                coord = positions[color][1]
+                goal = transform_board_to_screen(but)
+                if(goal == (coord[0]+1,coord[1]+2)):
                     return 1
-                else:#decide which one can be moved here
-                    coord = positions[1][1]
-                    goal = transform_board_to_screen(but)
-                    if(goal == (coord[0]+1,coord[1]+2)):
-                        return 1
-                    elif (goal == (coord[0]+2,coord[1]+1)):
-                        return 1
-                    elif (goal == (coord[0]+2,coord[1]-1)):
-                        return 1
-                    elif (goal == (coord[0]+1,coord[1]-2)):
-                        return 1
-                    elif (goal == (coord[0]-1,coord[1]-2)):
-                        return 1
-                    elif (goal == (coord[0]-2,coord[1]-1)):
-                        return 1
-                    elif (goal == (coord[0]-2,coord[1]+1)):
-                        return 1
-                    elif (goal == (coord[0]-1,coord[1]+2)):
-                        return 1
-                    else:
-                        return 6
+                elif (goal == (coord[0]+2,coord[1]+1)):
+                    return 1
+                elif (goal == (coord[0]+2,coord[1]-1)):
+                    return 1
+                elif (goal == (coord[0]+1,coord[1]-2)):
+                    return 1
+                elif (goal == (coord[0]-1,coord[1]-2)):
+                    return 1
+                elif (goal == (coord[0]-2,coord[1]-1)):
+                    return 1
+                elif (goal == (coord[0]-2,coord[1]+1)):
+                    return 1
+                elif (goal == (coord[0]-1,coord[1]+2)):
+                    return 1
+                else:
+                    return 6
 
     elif (letter =='B'):
         if(color==0):
